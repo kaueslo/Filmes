c62f76d0b9eadc869d5321eddd727c9b9310b74f
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-03 21:58:52 -0300

    Criando a controller da nossa API

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,21 @@
+﻿using Filmes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class FilmeController : ControllerBase
+	{
+		private static List<Filme> filmes = new List<Filme>();
+
+		[HttpPost("AdicionaFilme")]
+		public void AdicionaFilme([FromBody]Filme filme)
+		{
+			filmes.Add(filme);
+
+			Console.WriteLine(filme.Titulo);
+		}
+
+	}
+}

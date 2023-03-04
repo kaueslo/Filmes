8aaafb40d0cb7e750d756df1874cd527234d41b9
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-04 00:13:23 -0300

    Realizando a conexao com o banco

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using Filmes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes.Data
+{
+	public class FilmeContext : DbContext
+	{
+		public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+		{
+
+		}
+
+		public DbSet<Filme> Filmes { get; set; }
+	}
+}
